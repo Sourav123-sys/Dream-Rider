93fef44ac447bb77de54741b262e1404f85ed8ef
Sourav123 <khansourav58@gmail.com>
2021-09-21 11:43:34 +0600

    first commit

---
+++ src/Components/RegisterForm/firbase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBgCWb5ha_OgiKPss7qcTHcyhgTho4DyhI",
+  authDomain: "dream-rider-9ddc6.firebaseapp.com",
+  projectId: "dream-rider-9ddc6",
+  storageBucket: "dream-rider-9ddc6.appspot.com",
+  messagingSenderId: "508588263818",
+  appId: "1:508588263818:web:6987c3c487f22784786073"
+};
+
+export default firebaseConfig;
